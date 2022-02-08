6c229c164a93fbd6caf4088e5dc204358ded6868
Lexa1488Ruskiy4elovek <Alex1998Kuznecov@yandex.ru>
2022-02-08 14:33:20 +0700

    homework #3

---
+++ Task_3.dart
@@ -0,0 +1,11 @@
+void main() {
+  int a = 1705;
+  int b = 1900;
+  int c = 1601;
+  int d = 2000;
+
+  print((a ~/ 100) + 1);
+  print((b ~/ 100));
+  print((c ~/ 100) + 1);
+  print((d ~/ 100));
+}
